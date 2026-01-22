2887e9c3591f12761c9ba5cd818d6f56bb2740ab
hugo <hugo.marian.etu@univ-lille.fr>
2026-01-22 11:37:14 +0100

    Correction automatisation

--- fichiers/midi2tokens.py
+++ fichiers/midi2tokens.py
@@ -1,11 +1,11 @@
 import miditoolkit
 import sys
 
-nomFichier="GrandMidiPiano/Bach, Johann Sebastian, Partita in G major, BWV 829, aecenXn3obw.mid"
+nomFichier="../GrandMidiPiano/Bach, Johann Sebastian, Partita in G major, BWV 829, aecenXn3obw.mid"
 midi = miditoolkit.MidiFile(nomFichier)
-if(sys.argv[1]):
+try:
     sortie = sys.argv[1]
-else:
+except:
     sortie = "test.txt"
 
 
@@ -15,6 +15,7 @@ for inst in midi.instruments:
     for note in inst.notes:
         tokens.append(f"POSITION_{note.start}")
         tokens.append(f"NOTE_ON_{note.pitch}")
+        tokens.append(f"VELOCITY_{note.pitch}")
         tokens.append(f"DURATION_{note.end - note.start}")
 
 with open(nomFichier, "w", encoding="utf-8") as f:
